50e6ec22841256bfe59ad0c02013d61e56b34c7e
aliakseividaseu <alexaat@yahoo.co.uk>
2023-10-02 21:43:05 +0100

    Clean Up

--- app.js
+++ app.js
@@ -1,75 +1,19 @@
-const dialog = document.querySelector("dialog"); 
+const dialog = document.querySelector(".dialog"); 
 const submitButton = document.querySelector('.button-container');
 const dialogTitle = document.querySelector('.dialog-title');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
-const closeDialogButton = document.querySelector('.dialog-header img');
-
 
 submitButton.addEventListener('click', e => {
-    const name = nameInput.value.trim();
-    if(name){
+    const name = nameInput.value.trim();   
+    if(name != ''){      
         dialogTitle.textContent = `Thanks for signin up, ${name}`;
-        dialog.show(); 
+        dialog.style.display='flex';
 
+        setTimeout(() => {
+            dialog.style.display='none';
+            nameInput.value = '';
+            emailInput.value = '';
+        }, 3000);
     }
 });
-
-closeDialogButton.addEventListener('click', e=> {
-    dialog.close();
-    nameInput.value = '';
-    emailInput.value = '';
-
-});
-
-// const textFields = document.querySelectorAll('.text-field-input');
-
-
-
-// textFields.forEach(textFieldInput => {
-//     textFieldInput.addEventListener('click', e => {
-//         const target = e.currentTarget;
-//         const lable = target.querySelector('.text-field-lable');
-//         const input = target.querySelector('.text-field-text-input');
-
-//         const button = target.parentElement.querySelector('.text-field-image');
-        
-//         button.style.display = 'flex';
-
-//         lable.style.fontSize = '0.8rem';
-//         input.style.display = 'block';
-//         input.focus();
-
-//         input.addEventListener('change', e=>{
-//             lable.textContent = e.target.value;
-//         });
-
-//         target.addEventListener('focusout', e=>{
-//             lable.style.fontSize = '1.1rem';
-//             input.style.display = 'none';
-//             // button.style.display='none';
-//         });
-//     });
-// });
-
-// const clearButtons = document.querySelectorAll('.text-field-image-cross');
-// clearButtons.forEach(button => {
-//     console.log(button)
-//     button.addEventListener('click', e => {
-//         console.log('clear')
-//         const input = e.target.parentElement.parentElement.querySelector('.text-field-text-input');
-//         input.value = '';
-//         const lable = e.target.parentElement.parentElement.querySelector('.text-field-lable');
-//         lable.textContent = 'Display Name';
-
-
-//     })
-// })
-
-
-
-
-
-
-
-
